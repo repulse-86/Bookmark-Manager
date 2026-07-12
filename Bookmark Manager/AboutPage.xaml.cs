65855d49fc431e155dc5efa9f40aa7d08744660a
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2026-07-12 23:11:09 +0800

    feat: add about page with Ko-fi support link

---
+++ Bookmark Manager/AboutPage.xaml.cs
@@ -0,0 +1,49 @@
+using Bookmark_Manager.Common;
+using System;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Bookmark_Manager
+{
+    public sealed partial class AboutPage : Page
+    {
+        private readonly NavigationHelper navigationHelper;
+
+        public AboutPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            SetVersion();
+        }
+
+        private void SetVersion()
+        {
+            var package = Windows.ApplicationModel.Package.Current;
+            var version = package.Id.Version;
+            VersionTextBlock.Text = $"Version {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        private async void SupportButton_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("https://ko-fi.com/nextcall");
+            await Launcher.LaunchUriAsync(uri);
+        }
+    }
+}
